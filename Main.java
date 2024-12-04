31e0f39bdff23737843e8556cb80c4e1d47c3cb3
Abdi Guled <133576011+AhmedGuled@users.noreply.github.com>
2024-12-04 17:05:37 -0500

    Main Changes

--- Main.java
+++ Main.java
@@ -1,47 +1,51 @@
 class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        BST<Integer> lab5Tree = new BST<>();
+    // Step 1 - Create a BST tree object called lab5Tree
+    BST<Integer> lab5Tree = new BST<>();
 
-        lab5Tree.insert(13);
-        lab5Tree.insert(22);
-        lab5Tree.insert(36);
-        lab5Tree.insert(5);
-        lab5Tree.insert(48);
-        lab5Tree.insert(17);
-        lab5Tree.insert(39);
-        lab5Tree.insert(2);
-        lab5Tree.insert(26);
-        lab5Tree.insert(40);
-        lab5Tree.insert(29);
-        lab5Tree.insert(34);
-        lab5Tree.insert(10);
+    // Step 2 - Insert the following values: 13, 22, 36, 5, 48, 17, 39, 2, 26, 40, 29, 34, 10
+    lab5Tree.insert(13);
+    lab5Tree.insert(22);
+    lab5Tree.insert(36);
+    lab5Tree.insert(5);
+    lab5Tree.insert(48);
+    lab5Tree.insert(17);
+    lab5Tree.insert(39);
+    lab5Tree.insert(2);
+    lab5Tree.insert(26);
+    lab5Tree.insert(40);
+    lab5Tree.insert(29);
+    lab5Tree.insert(34);
+    lab5Tree.insert(10);
 
-        // Delete the value 17
-        lab5Tree.delete(17);
+    // Step 3 - Delete the value 17
+    lab5Tree.delete(17);
 
-        // Traverse and output the values using inorder (sorted)
-        lab5Tree.preorder();
+    // Step 4 - Traverse and output the values using inorder (sorted)
+    lab5Tree.preorder();
 
-        // Traverse and output the values using postorder
-        lab5Tree.postorder();
+    // Step 5 - Traverse and output the values using postorder
+    lab5Tree.postorder();
 
-        // Traverse and output the values using preorder
-        lab5Tree.preorder();
+    // Step 6 - Traverse and output the values using preorder
+    lab5Tree.preorder();
 
-        // Display the result of a search for the value 36
-        System.out.println(lab5Tree.search(36));
+    // Step 7 - Display the result of a search for the value 36
+    System.out.println(lab5Tree.search(36));
 
-        // Display the result of a search for the value 37
-        System.out.println(lab5Tree.search(37));
+    // Step 8 - Display the result of a search for the value 37
+    System.out.println(lab5Tree.search(37));
 
-        for (BST.TreeNode<Integer> node : lab5Tree.path(2)) {
-            System.out.print(node.element + " ");
-        }
-
-        for (BST.TreeNode<Integer> node : lab5Tree.path(34)) {
-            System.out.print(node.element + " ");
-        }
-
+    // Step 9 - Using the path() method, display the path from the root to 2
+    for (BST.TreeNode<Integer> node : lab5Tree.path(2)) {
+      System.out.print(node.element + " ");
     }
+
+    // Step 10 - Display the path from the root to 34
+    for (BST.TreeNode<Integer> node : lab5Tree.path(34)) {
+      System.out.print(node.element + " ");
+    }
+
+  }
 }
